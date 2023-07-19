4008c522999ace9e01e59d25bebd24229b7160df
Shruti Vats <shrutivats.0503@gmail.com>
2023-07-19 14:23:52 +0530

    this keyword stating now

--- constructor.cpp
+++ constructor.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
 
+
+//whenever an object is created , a constructor is called
+//constructor -> 1. np return type, 2. can have no input parameter
+class Hero{
+    public:
+    Hero(){
+        cout<<"Constructor called"<<endl;
+    }
+};
+
+
 int main(){
-    
+    cout<<"Hello"<<endl;
+    //object created statically
+    Hero h1; //constructor called by this command
+
+    //dynamic
+    Hero *h = new Hero();
+
 }
