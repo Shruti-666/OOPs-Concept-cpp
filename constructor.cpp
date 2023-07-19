c4761b75e87e2656de0b4960729aa61a74c4d9b6
Shruti Vats <shrutivats.0503@gmail.com>
2023-07-19 14:37:28 +0530

    starting copy constructor

--- constructor.cpp
+++ constructor.cpp
@@ -5,18 +5,37 @@ using namespace std;
 //whenever an object is created , a constructor is called
 //constructor -> 1. np return type, 2. can have no input parameter
 class Hero{
+    int health,level;
     public:
-    Hero(){
+    Hero(){//default constructor
         cout<<"Constructor called"<<endl;
     }
+
+    //parameterized constructor
+    //'this' stores the address of current object 
+    //this is a pointer to the current object
+    Hero(int health){
+        cout<<"this -> "<<this<<endl;
+        this->health=health;
+    }
+
+    Hero(int health,int level){
+        this->health=health;
+        this->level=level;
+    }
+
+
 };
 
 
 int main(){
     cout<<"Hello"<<endl;
+    
     //object created statically
-    Hero h1; //constructor called by this command
+    Hero h1(10); //constructor called by this command
+    cout<<"Address of Hero : "<<&h1<<endl;
 
+    Hero temp(22,2);
     //dynamic
     Hero *h = new Hero();
 
